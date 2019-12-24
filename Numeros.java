b383916a86f3e1247cd776db83799c8bef9b6303
Jorge Stiven Pachon Garcia <jopachon@personalsoft.com.co>
2019-12-23 21:22:11 -0500

    Se agregan los archivos base del commit inicial.

---
+++ Numeros.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+public class Numeros {
+    public static void main(String[] args) {
+        int a = 201_123;
+        byte b = (byte)a;
+        System.out.println(b);
+
+        float a1 = 3.5f;
+        float b1 = (float)3.5;
+        System.out.println(b1);
+
+        char myUnicodeSymbol = '\uD834';
+        System.out.println(myUnicodeSymbol);
+
+    }
+}
